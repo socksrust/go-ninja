089fbdfff74195c3026e55604cfd25cde0065c87
jaburcodes <guilhermejabur@outlook.com>
2018-01-29 01:26:21 -0200

    add go ninja input

--- app/components/red-input.js
+++ app/components/red-input.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { InputText } from 'react-native'
+import { TextInput } from 'react-native'
 import styled from 'styled-components/native'
+import theme from '../utils/theme'
+const Input = styled.TextInput`
+  width: 100%;
+  border-bottom-width: 1px;
 
+`
 
-const RedInput = () => (
-  <InputText 
-      
+const RedInput = props => (
+  <Input {...props}
+    selectionColor={theme.red}
+    underlineColorAndroid={theme.red}
   />
 )
 
