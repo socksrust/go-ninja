64e4b802abf9f202144d2367f0ce2d49a0f30581
jaburcodes <guilhermejabur@outlook.com>
2018-01-29 01:26:47 -0200

    update navigation

--- RootNavigator.js
+++ RootNavigator.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 
-import Login from './app/scenes/Home'
+import Login from './app/scenes/Login'
 import { StackNavigator } from 'react-navigation';
 
 const RootNavigator = StackNavigator({
